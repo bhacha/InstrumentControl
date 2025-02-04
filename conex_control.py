04503317dc3048cf9eae9cf0c2f7153561d67c51
bhacha <29590546+bhacha@users.noreply.github.com>
2025-02-04 17:59:56 -0600

    conex updates

--- conex_control.py
+++ conex_control.py
@@ -3,7 +3,7 @@ import sys
 import time
 
 
-#sys.path.append(r'C:/Program Files (x86)/Newport/MotionControl/CONEX-CC/Bin')
+sys.path.append(r'C:/Program Files (x86)/Newport/MotionControl/CONEX-CC/Bin')
 
 clr.AddReference("Newport.CONEXCC.CommandInterface")
 
@@ -16,44 +16,110 @@ class Actuator:
                  com_port: str):
         self.com_port = com_port
         self.driver = ConexCC()
+        self.OpenInstrument()
 
+    def TP(self):
+        """
+        Get current position
+        It's probably better to use one of the "Get" methods because this does not seem to update as frequently. 
+
+        Returns
+        --------
+        position : float
+            current stage position
+        """
+        resp, position, err = self.driver.TP(1) 
+        if resp == 0:
+            print(f"Position: {position}")
+            return position
+        else:
+            print(f"Error getting status of device: {err}")
 
     def OpenInstrument(self):
+        """
+        Establish connection to instrument
+        """
+
         resp = self.driver.OpenInstrument(self.com_port)
         if resp == 0:
+            print("Connection established")
             pass
         else:
             print(f"Error opening connection with device: {resp}")
 
     def PA_Get(self):
-        target, resp = self.driver.PA_Get(1)
+        """
+        Redundant for TP, as far as I can tell.
+
+        Returns
+        --------
+        target : float
+            current stage position
+        
+        """
+        resp, target, err = self.driver.PA_Get(1)
         if resp == 0:
             print(f"Current Position: {target}")
         else:
-            print(f"Error returning absolution position: {resp}")
+            print(f"Error returning absolute position: {err}")
 
     def PA_Set(self, position):
-        resp = self.driver.PA_Set(1, position)
+        """
+        Move the stage to position.
+
+        Parameters
+        ----------
+        step : float
+            position to move to    
+        
+        """
+        resp, err = self.driver.PA_Set(1, position)
         if resp == 0:
+            print("Moved")
             pass
         else:
-            print(f"Error moving absolute: {resp}")
+            print(f"Error moving absolute: {err}")
 
     def PR_Get(self):
-        step, resp = self.driver.PR_Get(1)
+        """
+        Retrieve the current position after relative move
+
+        Returns
+        --------
+        position : float
+            current position after relative move
+        
+        """
+        resp, position, err = self.driver.PR_Get(1)
         if resp == 0:
-            print(f"Current Step Size: {step}")
+            print(f"Current Position: {position}")
         else:
-            print(f"Error returning relative position: {resp}")
+            print(f"Error returning relative position: {err}")
 
     def PR_Set(self, step):
-        resp = self.driver.PA_Set(1, step)
+        """
+        Move the stage by step.
+
+        Parameters
+        ----------
+        step : float
+            distance to move stage       
+        """
+        resp, err = self.driver.PR_Set(1, step)
         if resp == 0:
+            print(f"Moved approx. {step}")
             pass
         else:
-            print(f"Error moving relative: {resp} ")
+            print(f"Error moving relative: {err} ")
+
+    def CloseInstrument(self):
+        resp = self.driver.CloseInstrument()
+        if resp == 0:
+            print("Connection Closed")
+            pass
+        else:
+            print(f"Error closing connection with device: {resp}")
 
 
 if __name__ == "__main__":
-
-    actuator = Actuator("com3")
+    pass
