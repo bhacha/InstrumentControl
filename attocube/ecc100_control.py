d0fe10a0e314d11eb5526653ccc10b73f3f6a650
29590546+bhacha@users.noreply.github.com <29590546+bhacha@users.noreply.github.com>
2025-04-07 13:06:00 -0500

    use local library modified:   attocube/ecc100_control.py

--- attocube/ecc100_control.py
+++ attocube/ecc100_control.py
@@ -2,7 +2,7 @@ from ctypes import (c_int32, c_bool, byref, create_string_buffer, Structure, POI
 from enum import Enum
 import time
 
-lib=oledll.LoadLibrary('X:\dataBackup\Programs\Attocube\Software_ECC100_1.6.8\ECC100_DLL\Win64\ecc.dll') # this is the path to the ecc.dll, if becoming a package easily can be modified
+lib=oledll.LoadLibrary('InstrumentControl\dependencies\ECC100\ecc.dll') # this is the path to the ecc.dll, if becoming a package easily can be modified
 
 class EccInfo(Structure):
     _fields_ = [('id',c_int32),('locked',c_bool)]
